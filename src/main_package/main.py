31adb50f28a00555b66a7c402fc156ee8d217fe7
Tobi <ulkmv@student.kit.edu>
2023-10-18 22:50:44 +0200

    draw polygon

--- src/main_package/main.py
+++ src/main_package/main.py
@@ -1,24 +1,35 @@
 import pygame
 import sys
-from model_package.hexagon import Hexagon
+sys.path.append("C:\\Users\\tobia\\OneDrive\\Desktop\\Programmierprojekte\\OhMutterGame\\src")
 from model_package.hexagon import Hexagons
-sys.path.append("C:\\Users\\tobia\\OneDrive\\Projects\\OhMutter\\src\\model_package")
 
-pygame.init()
+
+def main():
+    
+
+    
+    pygame.init()
 
 # Create a Pygame window
-screen = pygame.display.set_mode((900, 700))
+    screen = pygame.display.set_mode((900, 700))
+    hex = Hexagons(screen=screen)
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    hex = Hexagon()
+    running = True
+    while running:
+        hex.draw_regular_polygon(screen)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
    
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+
--- src/model_package/hexagon.py
+++ src/model_package/hexagon.py
@@ -1,8 +1,11 @@
 import pygame
 import math
+import sys
+import numpy as np
 class Hexagons():
     def __init__(self, screen):
         # Create all the Hexagons exactly as in the original game
+        sys.path.append("C:\\Users\\tobia\\OneDrive\\Desktop\\Programmierprojekte\\OhMutterGame\\src")
 
         self.hexagons = {}
         self.hexagons[1] = Hexagon(id=1, top_num=1, top_right_num=6, bottom_right_num=5, bottom_num=3,bottom_left_num=2,top_left_num=4)
@@ -18,17 +21,18 @@ class Hexagons():
         self.font = pygame.font.Font(None, 36)
         self.side_length = 50
         self.screen = screen
-    def calculate_vertices(self):
-        vertices = []
-        for i in range(6):
-            angle = math.radians(60 * i)
-            x = 50 + self.side_length * math.cos(angle)
-            y = 50 + self.side_length * math.sin(angle)
-            vertices.append((x, y))
-        return vertices
-    def draw_hexagon(self,id, screen):
-        hexagon = self.hexagons[id]
-        pygame.draw.polygon(self.screen, self.blue,)
+
+    def draw_regular_polygon(self, screen):
+        corners, radius = 6, 70
+        print((radius * math.cos(180/corners)))
+        x, y = (900 - radius - 20), np.round(700  - (radius * (math.cos(math.pi/corners))) - 20)
+        print(x)
+        print(y)
+        width = 0  # If width = 0, it will fill the polygon
+        y_height = 10
+        pygame.draw.polygon(screen, self.blue, [(x + radius * math.cos(2 * math.pi * i / corners), y + radius * math.sin(2 * math.pi * i / corners)) for i in range(corners)], width)
+
+
 
 
 
